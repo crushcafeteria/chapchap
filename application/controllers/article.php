078236eb5c50e7cc5e05b821b1c3d86e7758cf45
Nelson Ameyo <doe@nelson-pc.(none)>
2014-05-22 10:14:04 +0300

    Built articles feature

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -5,9 +5,9 @@ class Article extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model('docs/book_model');
-		$this->load->model('docs/chapter_model');
-		$this->load->model('docs/article_model');
+		$this->load->model('book_model');
+		$this->load->model('chapter_model');
+		$this->load->model('article_model');
 		$this->load->library('form_validation');
 	}
 
@@ -21,10 +21,28 @@ class Article extends CI_Controller {
 	public function write($chapterID){
 
 		$data['title'] = 'New Article';
-		$data['chapterID'] = $chapterID;
+		$data['chapter'] = $this->chapter_model->findChapter($chapterID);
+
+		$this->form_validation->set_rules('title', 'title', 'required|trim');
+		$this->form_validation->set_rules('content', 'content', 'required|trim');
+
+		$this->form_validation->set_error_delimiters('<div class="form-error text-danger">', '</div>');
+
 
 		if(!$this->form_validation->run()){
-			$this->template->inject('docs/article/write', $data);
+			$this->template->inject('article/write', $data);
+		} else {
+			// Save article
+
+			if($this->article_model->createNewArticle($this->input->post())){
+				redirect('article/view/'.$chapterID.'?status=article_created');
+			} else {
+				$data['msgBox'] = $this->arena->renderMsgBox('An error occured while saving this article. Please try again later', 'Database Error');
+				$this->template->inject('article/write', $data);
+			}
+
+			// var_dump($this->input->post());
+
 		}
 	}
 
@@ -36,10 +54,69 @@ class Article extends CI_Controller {
 		$data['title'] = $this->arena->titleCase($data['chapter']['name']);
 		$data['articles'] = $this->article_model->getAllArticles();
 
-		$this->template->inject('docs/article/view', $data);
+		$this->template->inject('article/view', $data);
 
 		// var_dump($book);
 		
+	}
+
+
+	/*Displays an article*/
+	public function read($articleID){
+
+		$data['article'] = $this->article_model->findArticle($articleID);
+		$data['title'] = $data['article']['title'];
+
+		$this->template->inject('article/read', $data);
+
+	}
+
+
+	/*Edit an article*/
+	public function edit($articleID){
+
+		$data['title'] = 'Edit Article';
+		$data['article'] = $this->article_model->findArticle($articleID);
+
+		$this->form_validation->set_rules('title', 'title', 'required|trim');
+		$this->form_validation->set_rules('content', 'content', 'required|trim');
+
+		$this->form_validation->set_error_delimiters('<div class="form-error text-danger">', '</div>');
+
+
+		if(!$this->form_validation->run()){
+			$this->template->inject('article/edit', $data);
+		} else {
+			// Save edited article
+
+			// var_dump($this->input->post());
+
+			// exit();
+
+			if($this->article_model->editArticle($this->input->post())){
+				redirect('article/read/'.$this->input->post('article_id').'?status=article_edited');
+			} else {
+				$data['msgBox'] = $this->arena->renderMsgBox('An error occured while saving this article. Please try again later', 'Database Error');
+				$this->template->inject('article/edit', $data);
+			}
+
+			// var_dump($this->input->post());
+
+		}
+	}
+
+
+	/*Delete an article*/
+
+	function delete($articleID){
+
+		$chapterID = $this->article_model->deleteArticle($articleID);
+
+		if(!$chapterID){
+			// redirect('article/view/'.$) DO SOMETHING HERE TODO
+		} else {
+			redirect(base_url('article/view/'.$chapterID.'?status=article_deleted'));
+		}
 	}
 
 }
