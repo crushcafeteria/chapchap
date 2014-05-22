50380e45f44002142e2ad5116ed6741266f022b1
Nelson Ameyo <doe@nelson-pc.(none)>
2014-05-22 10:20:01 +0300

    Setup CKeditor to write articles

--- application/views/article/write.php
+++ application/views/article/write.php
@@ -1,14 +1,18 @@
 <div class="col-md-12">
 
-	<legend>Write a new article</legend>
+	<legend>New article under <span title="<?=substr($chapter['description'], 0, 150)?>..."><?=$chapter['name']?></span></legend>
 
-	<?=form_open(base_url('docs/article/write/'.$chapterID))?>
+	<?=form_open(base_url('article/write/'.$chapter['id']))?>
+
+	<?=form_hidden('chapter_id', $chapter['id'])?>
 
 	<?=form_label('Title', 'title')?>
-	<?=form_input('title', '', 'class="form-control" placeholder="Article title..."')?>
+	<?=form_input('title', set_value('title'), 'class="form-control" placeholder="Article title..."')?>
+	<?=form_error('title')?>
 
-	<?=form_label('Body', 'body')?>
-	<textarea id="content" name="body"></textarea>
+	<?=form_label('Content', 'content')?>
+	<textarea id="content" name="content"><?=set_value('content')?></textarea>
+	<?=form_error('content')?>
 
 	<br>
 
@@ -21,6 +25,11 @@
 
 <script>
 CKEDITOR.replace('content', {
-	height : 350
+	height : 350,
+	skin: 'office'
+});
+
+$('[title]').tooltip({
+	placement: 'right'
 });
 </script>
