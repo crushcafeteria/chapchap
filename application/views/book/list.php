2cfd1560259b1715d0986cc4528038e4a1798b89
Nelson Ameyo <doe@nelson-pc.(none)>
2014-05-22 10:19:26 +0300

    Added listing books

---
+++ application/views/book/list.php
@@ -0,0 +1,52 @@
+<div class="col-md-9">
+	<legend>Welcome to Arena Docs</legend>
+
+	<?php
+
+	if(@$_GET['status']=='success'){
+		$this->arena->msgBox('A new book has been successfully created', 'Success', 'alert-success');
+	}
+
+	// var_dump($books);
+
+	?>
+
+	<?php if(count($books)==0):?>
+		<div class="text-muted lead text-center">
+			There are no books
+		</div>
+	<?php else:?>
+
+		<div class="list-group">
+			<?php foreach ($books as $book):?>
+					
+				<a href="<?=base_url('chapter/view/'.$book['id'])?>" class="list-group-item">
+
+					<small class="pull-right text-muted">
+						<?=$this->arena->fbTime($book['created_on'])?>
+					</small>
+
+					<h4 class="text-info" style="border-bottom: 1px solid #f5f5f5;"><?=$book['name']?></h4>
+					<div style="text-align: justify;">
+						<?=$this->arena->sentenceCase($book['description'])?>
+					</div>
+
+					<button type="button" class="btn btn-primary btn-sm">button</button>
+
+					
+
+					
+				</a>
+			<?php endforeach;?>
+		</div>
+
+	<?php endif;?>
+
+</div>
+
+
+<div class="col-md-3 pull-right">
+	<legend>Options</legend>
+
+	<a href="<?=base_url('book/add')?>" class="btn btn-default btn-block">New Book</a>
+</div>
